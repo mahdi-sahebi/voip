cee3b33c38e2b74cb5f1f425a3ce3e83c29f53ec
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2026-01-08 05:28:51 +0000

    [TEST]: Test cases of making call are defined

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,17 @@
+/* TODO(MN): Tests of timeouts. Invalid uri, ...
+ */
+
 #include <memory>
 #include <stdexcept>
+#include <thread>
+#include <chrono>
 #include <gtest/gtest.h>
 
 using namespace std;
+using namespace std::this_thread;
+using namespace std::chrono;
+using namespace std::chrono_literals;
+
 
 TEST(creation, invalid)
 {
@@ -40,14 +49,49 @@ TEST(register_account, with_pbx)
     );
 }
 
+// TODO(MN): Use Fixture
+static void setup(Call& call)
+{
+    ASSERT_NO_THROW(
+        call->start();
+    );
+    ASSERT_TRUE(call->registerAccount("sip:701@192.168.0.100", "701", "pass", 1000));
+}
+
 TEST(call, not_existing)
 {
-    FAIL();
+    constexpr auto TEST_COUNT{3};
+    auto call = make_unique<Call>();
+    setup(call);
+
+    EXPECT_NO_THROW(
+        for (uint8_t index = 0; index < TEST_COUNT; index++) {
+            EXCEPT_FALSE(call->makeCall("", 200);
+            EXCEPT_FALSE(call->makeCall("invalid_uri", 200);
+        }
+    );
+
+    EXPECT_NO_THROW(
+        call->stop();
+    );
 }
 
 TEST(call, existing)
 {
-    FAIL();
+    constexpr auto TEST_COUNT{3};
+    auto call = make_unique<Call>();
+    setup(call);
+
+    EXPECT_NO_THROW(
+        for (uint8_t index = 0; index < TEST_COUNT; index++) {
+            EXCEPT_TRUE(call->makeCall("sip:700@192.168.0.100", 1000);
+            sleep_for(1s);
+        }
+    );
+
+    EXPECT_NO_THROW(
+        call->stop();
+    );
 }
 
 TEST(hold, caller)
